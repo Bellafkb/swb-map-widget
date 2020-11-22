7e98061b18241534b0a29bf64c2c4acd58745d05
Bilal Bellafkir <bilal.bellafkir@informatik.hu-berlin.de>
2020-11-22 13:03:10 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+function App({ domElement }) {
+  return (
+    <div className="reddit_widget__app">
+      hello world
+    </div>
+  );
+}
+
+export default App;
